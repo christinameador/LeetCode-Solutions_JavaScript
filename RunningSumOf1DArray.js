d6e7b4cb3b606494d0c9f8fcc4099b8981ef92af
Christina Meador <christinameador1@gmail.com>
2020-09-09 07:12:29 -0500

    Delete RunningSumOf1DArray.js

--- RunningSumOf1DArray.js
+++
@@ -1,9 +0,0 @@
-var runningSum = function(nums) {
-    let sum = 0;
-    let finalArray = [];
-    for (let i=0; i<nums.length; i++) {
-        sum += nums[i];
-        finalArray.push(sum);
-    }
-    return finalArray;
-};
