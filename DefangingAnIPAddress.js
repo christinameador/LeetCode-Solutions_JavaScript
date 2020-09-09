0659fb500cc0e6f88672c2faf2bb0be6c8bd57fa
Christina Meador <christinameador1@gmail.com>
2020-09-09 07:12:21 -0500

    Delete DefangingAnIPAddress.js

--- DefangingAnIPAddress.js
+++
@@ -1,6 +0,0 @@
-var defangIPaddr = function(address) {
-    if (!address || address.length == 0) return "";
-    let addressArray = address.split('.');
-    let defangedAddress = addressArray.join('[.]');
-    return defangedAddress;
-};
